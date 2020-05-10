f91007c0f1d25a8c527c262c3beb8ff4f3635690
Andrew Winnicki <andrew.winnicki.sc@gmail.com>
2020-05-10 12:16:27 -1000

    added velocities distributed maxwell-boltmann for all three dimensions

--- mc_particle_trajectory.py
+++ mc_particle_trajectory.py
@@ -1,7 +1,79 @@
 import numpy as np
+import time
+import threading
+import sys
+
 from scipy.stats import maxwell
 
+# -----------------------------------------------------------------------------
+# initialize variables
+# -----------------------------------------------------------------------------
+
+# number of particles
+n = 10
+
+# time and timestep
+t = 0
+dt = 0.1
+
+# distance from cell to 4K aperture; will be using mm
+l = 100
+
+# particle collection
+ptcls = []
+
+# -----------------------------------------------------------------------------
+# # initialize N molecules with velocity distribution at position 0, t=0
+# -----------------------------------------------------------------------------
+
+class Point():
+    def __init__(self, coords, speed, mass):
+        self.coords = coords
+        self.speed = speed
+        self.mass = mass
+        self.acc = np.array([0, 0, 0])
+
+    # define movement
+    def move(self, dt):
+        self.coords = self.coords + self.speed * dt
+
+    def accelerate(self, dt):
+        self.speed = self.speed + self.acc * dt
+
+print('Generating {} particles...'.format(n))
+
+for i in range(n):
+    ptcls.append(Point(np.array([0.0, 0.0, 0.0]), np.array([maxwell.rvs(), \
+        maxwell.rvs(), maxwell.rvs()]), 1.0))
+    print('Particle {} has coordinates, speed, mass: '.format(i), \
+        ptcls[i].coords, ', ', ptcls[i].speed, ', ', ptcls[i].mass)
+
+# -----------------------------------------------------------------------------
+# kinematic propagation
+# -----------------------------------------------------------------------------
+
+# will propagate for 100 mm, 0.1 s timesteps
+
+# -----------------------------------------------------------------------------
+# deflected by force
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# calculate how many molecules end up in the trap region
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# #######################
+# EXTRAS BELOW
+# #######################
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
 # potential vector definition; phased out in favor of numpy.array
+# -----------------------------------------------------------------------------
+
 # # define vector
 # class Vector(list):
 #     def __init__(self, *el):
@@ -35,35 +107,3 @@ from scipy.stats import maxwell
 # print(a + b)
 # print(a - b)
 # print(a % b)
-
-# initialize variables
-n = 10
-t = 0
-
-# initialize N molecules with velocity distribution at position 0, t=0
-
-class Point():
-    def __init__(self, coords, speed, mass):
-        self.coords = coords
-        self.speed = speed
-        self.mass = mass
-        self.acc = np.array([0, 0, 0])
-
-    # define movement
-    def move(self, dt):
-        self.coords = self.coords + self.speed * dt
-
-    def accelerate(self, dt):
-        self.speed = self.speed + self.acc * dt
-
-ptcls = []
-
-for i in range(n):
-    ptcls.append(Point(np.array([0.0, 0.0, 0.0]), maxwell.rvs(), 1.0))
-    print(ptcls[i].speed)
-
-# kinematic population
-
-# deflected by force
-
-# calculate how many molecules end up in the trap region
