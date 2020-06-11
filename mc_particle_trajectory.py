a1bbb99d1699dc3a4f283d483fd9db96ce90a3f9
Andrew Winnicki <andrew.winnicki.sc@gmail.com>
2020-06-11 10:46:00 -1000

    working on 3d plot, using list comprehension for mesh spacing

--- mc_particle_trajectory.py
+++ mc_particle_trajectory.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from astropy.io import fits
+from mpl_toolkits import mplot3d
 from scipy.stats import maxwell
 print('Done.')
 
@@ -14,8 +15,11 @@ print('Done.')
 # Number of particles
 n = 3
 
-# Spacing, must be same as variable n in halbach.nb Mathematica
-sp = 3
+# Mesh spacing, must be same as variable m in halbach.nb Mathematica code
+m = 3
+
+# Radius of circular Halbach array, same as in halbach.nb code
+R = 25.4
 
 # Time and timestep
 t = 0.0
@@ -30,12 +34,12 @@ g = 2.0
 mu_B = 9.274e-24
 
 # Mass of a single CaOCH3 molecule (in kg)
-m = 1.1789827e-22
+mass = 1.1789827e-22
 
 # vectorized velocity and acceleration
-p = []
-v = []
-a = []
+p = np.array([])
+v = np.array([])
+a = np.array([])
 
 # ----------------------------------------------------------------------------
 # # Initialize n molecules
@@ -46,94 +50,118 @@ a = []
 print('Generating particles...')
 
 for i in range(n):
-    p.append([0.0, 0.0, 0.0])
-    v.append([np.random.standard_normal(), \
+    p = np.append(p, [0.0, 0.0, 0.0])
+    v = np.append(v, [np.random.standard_normal(), \
         np.random.standard_normal(),\
         np.random.normal(loc=100.0, scale=1.0)])
-    a.append([0.0, 0.0, 0.0])
+    a = np.append(a, [0.0, 0.0, 0.0])
 print('Done.')
 
 # convert to numpy array
-p = np.array(p)
-v = np.array(v)
-a = np.array(a)
 print('Positions array: \n {} \n'.format(p))
 print('Velocities array: \n {} \n'.format(v))
 print('Accelerations array: \n {} \n'.format(a))
 
-print('Done.')
-
 # ----------------------------------------------------------------------------
-# Deflection by force field
+# Force Field
 # ----------------------------------------------------------------------------
 
 # Import matrices, source:
 # https://mathematica.stackexchange.com/questions/163685/
 # export-a-3d-array-from-mathematica-and-import-it-in-python-as-a-numpy-array
-hdul = fits.open('/Users/andrewwinnicki/desktop/andrew/2019-2020/Doyle Lab/Modeling Project/B-Matrix/bMatrix.fits')
-bMatrix = np.array([hdul[i].data for i in range(1)][0])
-gradBxMatrix = np.gradient(bMatrix, axis=0)
+hdulBxMatrix = fits.open('/Users/andrewwinnicki/desktop/andrew/2019-2020/Doyle Lab/Modeling Project/B-Matrix/bxMatrix.fits')
+hdulByMatrix = fits.open('/Users/andrewwinnicki/desktop/andrew/2019-2020/Doyle Lab/Modeling Project/B-Matrix/byMatrix.fits')
+hdulBzMatrix = fits.open('/Users/andrewwinnicki/desktop/andrew/2019-2020/Doyle Lab/Modeling Project/B-Matrix/bzMatrix.fits')
 
-# Calculate force field at each spacing
-forceField = np.array([[[g * mu_B * s * gradBxMatrix[i][j][k] \
-    for k in range(sp)] for j in range(sp)] for i in range(sp)])
+bxMatrix = np.array([hdulBxMatrix[0].data[0], hdulBxMatrix[0].data[1], \
+    hdulBxMatrix[0].data[2]])
+byMatrix = np.array([hdulByMatrix[0].data[0], hdulByMatrix[0].data[1], \
+    hdulByMatrix[0].data[2]])
+bzMatrix = np.array([hdulBzMatrix[0].data[0], hdulBzMatrix[0].data[1], \
+    hdulBzMatrix[0].data[2]])
 
-print("Old force field: \n {} \n ".format(forceField))
+# Take gradients of all matrices in x, y, z coordinates respectively
+gradBxMatrix = np.gradient(bxMatrix, axis=0)
+gradByMatrix = np.gradient(byMatrix, axis=1)
+gradBzMatrix = np.gradient(bzMatrix, axis=2)
 
-# Add F = 0 for anywhere we're not counting the magnetic lens' effect
-# by adding rows and columns to matrix
+# Splice together gradients of all matrices here to get force field w/ vectors
+gradBMatrix = np.stack([gradBxMatrix, gradByMatrix, gradBzMatrix], axis=3)
 
-# Spacing between cell aperture and circular Halbach array is 8x greater than
-# spacing within array; 18x greater between array and MOT
-for _ in range((sp - 1) * 4):
-    # Since the distance from 4K aperture to lens is 8x the length of the lens
-    # bore, we must have an additional (sp - 1)*8 points in the force field with
-    # value 0
-    forceField = np.insert(forceField, 0, [[0.0 for _ in range(sp)]\
-        for _ in range(sp)], axis=0)
+# Plot the 3D Matrix to visualize force field in space
+gradBMatrixFig = plt.figure()
+gradBMatrixAx = gradBMatrixFig.gca(projection='3d')
 
-# Add in 18x 0's in the force field between the lens and the MOT
+# Create mesh grid in 3D space for visualization purposes
+bxDataDraft = [[i] * m for i in np.linspace(-R/2, R/2, m)]
+byDataDraft = [[i] * m for i in np.linspace(-R/2, R/2, m)]
+# Todo: adjust bzDataDraft to include the additional appended 0's
+bzDataDraft = [[i] * m for i in np.linspace(-R/2, R/2, m)]
 
-print("New force field: \n {} \n ".format(forceField))
+# Flatten lists
+bxData = [item for sublist in bxDataDraft for item in sublist]
+byData = [item for sublist in byDataDraft for item in sublist]
+# Todo: adjust bzData to include the additional appended 0's
+bzData = [item for sublist in bzDataDraft for item in sublist]
+
+gradBMatrixAx.scatter3D(bxData, byData, bzData, cmap='Greens')
+plt.show()
+
+# # Add F = 0 for anywhere we're not counting the magnetic lens' effect
+# # by adding rows and columns to matrix
+
+# # Spacing between cell aperture and circular Halbach array is 8x greater than
+# # spacing within array; 18x greater between array and MOT
+# # Todo: fix the '* 1' to be '* 4'
+# for _ in range((m - 1) * 1):
+#     # Since the distance from 4K aperture to lens is 8x the length of the lens
+#     # bore, we must have an additional (m - 1)*8 points in the force field with
+#     # value 0
+#     forceField = np.insert(forceField, 0, [[0.0 for _ in range(m)]\
+#         for _ in range(m)], axis=0)
+
+# # Add in 18x 0's in the force field between the lens and the MOT
+# Todo: add the 0's in the force field after the lens
+
+# print("New force field: \n {} \n ".format(forceField))
 
 # ----------------------------------------------------------------------------
 # Kinematic propagation
 # ----------------------------------------------------------------------------
 
-print('Propagating...')
+# print('Propagating...')
 
-# Propagate for 5 s, 0.05 s timesteps
-for step in np.linspace(0, 1, num=100, endpoint=False):
+# # Evolve system through time
+# for step in np.linspace(0, 1, num=10, endpoint=False):
 
-    # Truncate positions to get approximate indices for force field matrix
-    # in acceleration calculations
-    positions = np.around(p)
+#     # Truncate positions to get approximate indices for force field matrix
+#     # in acceleration calculations
+#     positions = np.around(p)
 
-    # Change the accelerations:
-    # Find the coordinate in the force field that each particle is closest to.
-    for index in range(n):
-        position = positions[index]
-        try:
-            # Attempt to set new acceleration to the F/m given by force field's
-            # coordinate
-            a[index] = forceField[position] // m
-            print('Made it to the break loop. I need to reshape the array.')
-            print('I also need to have a force field that uses not the norm,\
-                but a vector for the force (need to do gradient on the\
-                3d vector')
-        except:
-            # Set new accelerations to [0, 0, 0] if outside the scope of our
-            # Force field's coordinates
-            a[index] = [0, 0, 0]
-            print('New acceleration outside scope of calculated coordinates. ' \
-                + 'New acceleration set to [0, 0, 0].')
-    print('New accelerations array: {}'.format(a))
+#     # Change the accelerations:
+#     # Find the coordinate in the force field that each particle is closest to.
+#     for index in range(n):
+#         position = positions[index]
+#         try:
+#             # Attempt to set new acceleration to the F/m given by force field's
+#             # coordinate
+#             a[index] = forceField[position] / m
 
-    # Change the velocities:
+#             # I need to reshape the array.
+#             # I also need to have a force field that uses not the norm,
+#             # but a vector for the force (need to do gradient on the
+#             # 3d vector')
+#             # Todo
+#         except:
+#             # Set new accelerations to [0, 0, 0] if outside the scope of our
+#             # Force field's coordinates
+#             a[index] = [0, 0, 0]
 
-    # Change the positions:
+#     # Change the velocities:
 
-print('Done.')
+#     # Change the positions:
+
+# print('Done.')
 
 # print('Plotting...')
 
@@ -157,9 +185,9 @@ print('Done.')
 # Calculate how many molecules end up in the trap region
 # ----------------------------------------------------------------------------
 
-print('Calculating success rate...')
-# TODO
-print('Done.')
+# print('Calculating success rate...')
+# # TODO
+# print('Done.')
 
 # # ----------------------------------------------------------------------------
 # # #############################################################################
