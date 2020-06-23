b-field working, gradient fixes wip

magnetic_lens_monte_carlo/vector_field.py
@@ -7,12 +7,12 @@ from init import *
 print('Generating vector fields...\n')
 
 # Import matrices
-hdulBxMatrix = fits.open('/Users/andrewwinnicki/Desktop/Andrew/2019-2020/Doyle Lab/Modeling Magnetic Lens/magnetic_lens_monte_carlo/b_matrix/bxMatrix.fits')
-hdulByMatrix = fits.open('/Users/andrewwinnicki/desktop/Andrew/2019-2020/Doyle Lab/Modeling Magnetic Lens/magnetic_lens_monte_carlo/b_matrix/byMatrix.fits')
-hdulBzMatrix = fits.open('/Users/andrewwinnicki/desktop/Andrew/2019-2020/Doyle Lab/Modeling Magnetic Lens/magnetic_lens_monte_carlo/b_matrix/bzMatrix.fits')
-hdulNormBMatrix = fits.open('/Users/andrewwinnicki/desktop/Andrew/2019-2020/Doyle Lab/Modeling Magnetic Lens/magnetic_lens_monte_carlo/b_matrix/normbMatrix.fits')
-hdulMxMatrixSlice = fits.open('/Users/andrewwinnicki/desktop/Andrew/2019-2020/Doyle Lab/Modeling Magnetic Lens/magnetic_lens_monte_carlo/m_matrix/mxMatrix.fits')
-hdulMyMatrixSlice = fits.open('/Users/andrewwinnicki/desktop/Andrew/2019-2020/Doyle Lab/Modeling Magnetic Lens/magnetic_lens_monte_carlo/m_matrix/myMatrix.fits')
+hdulBxMatrix = fits.open('/Users/andrewwinnicki/Desktop/Andrew/2019-2020/Doyle Lab/Modeling Magnetic Lens/magnetic_lens_monte_carlo/b_matrix/bxMatrix_{}.fits'.format(m))
+hdulByMatrix = fits.open('/Users/andrewwinnicki/desktop/Andrew/2019-2020/Doyle Lab/Modeling Magnetic Lens/magnetic_lens_monte_carlo/b_matrix/byMatrix_{}.fits'.format(m))
+hdulBzMatrix = fits.open('/Users/andrewwinnicki/desktop/Andrew/2019-2020/Doyle Lab/Modeling Magnetic Lens/magnetic_lens_monte_carlo/b_matrix/bzMatrix_{}.fits'.format(m))
+hdulNormBMatrix = fits.open('/Users/andrewwinnicki/desktop/Andrew/2019-2020/Doyle Lab/Modeling Magnetic Lens/magnetic_lens_monte_carlo/b_matrix/normbMatrix_{}.fits'.format(m))
+hdulMxMatrixSlice = fits.open('/Users/andrewwinnicki/desktop/Andrew/2019-2020/Doyle Lab/Modeling Magnetic Lens/magnetic_lens_monte_carlo/m_matrix/mxMatrix_{}.fits'.format(m))
+hdulMyMatrixSlice = fits.open('/Users/andrewwinnicki/desktop/Andrew/2019-2020/Doyle Lab/Modeling Magnetic Lens/magnetic_lens_monte_carlo/m_matrix/myMatrix_{}.fits'.format(m))
 
 bxMatrix = np.array([hdulBxMatrix[0].data[i] for i in range(m)])
 byMatrix = np.array([hdulByMatrix[0].data[i] for i in range(m)])

magnetic_lens_monte_carlo/visualize.py
@@ -88,9 +88,9 @@ x2d, y2d = np.meshgrid(np.linspace(-R/2, R/2, m), np.linspace(-R/2, R/2, m))
 gradBxMatrixSlice = gradNormBMatrix[0][:, :, int(m/2)]
 gradByMatrixSlice = gradNormBMatrix[1][:, :, int(m/2)]
 # todo: get the color right, graded by strength of field
-color = normBMatrix[:, :, int(m/2)]
+# color = normBMatrix[:, :, int(m/2)]
 
-forceFieldSlice2DAx.quiver(x2d, y2d, gradBxMatrixSlice, gradByMatrixSlice, color=color)
+forceFieldSlice2DAx.quiver(x2d, y2d, gradBxMatrixSlice, gradByMatrixSlice)
 
 forceFieldSlice2DAx.axis('equal')
 