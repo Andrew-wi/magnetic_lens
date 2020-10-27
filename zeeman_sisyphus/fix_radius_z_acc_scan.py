e4c16cb8d8abc70cf54596dd4165e7307712d165
Andrew Winnicki <andrew.winnicki.sc@gmail.com>
2020-10-26 16:52:04 -1000

    added b-field checking programs to zs code, WIP

---
+++ zeeman_sisyphus/fix_radius_z_acc_scan.py
@@ -0,0 +1,53 @@
+# ----------------------------------------------------------------------------
+# Acceleration Profile, Fixed Radius Z-Axis Scan
+# ----------------------------------------------------------------------------
+from dependencies import *
+from vector_field import *
+
+import seaborn as sns
+
+# set seaborn plotting style
+sns.set_style("darkgrid")
+
+# initialize values
+z_scan = np.linspace(0, 2*R/1e3, 50)
+thetas = np.linspace(0, 2*np.pi, 100)
+radius = R / 4 / 1e3
+
+# initialize arrays for storing acceleration
+radial_acc_mean = []
+
+# loop through z_scan
+for z_val in z_scan:
+    radial_acc_ind = []
+    for theta in thetas:
+        # get x and y values from radius and theta
+        x = radius * np.cos(theta)
+        y = radius * np.sin(theta)
+
+        # do interpolation
+        l = (2*R / 1e3) / (m - 1)
+        xCoord = round((R/1e3 + x) / l)
+        yCoord = round((R/1e3 + y) / l)
+        zCoord = round(z_val / l)
+
+        # take x, y, z coords and get acc; append to array
+        radial_acc_ind.append(np.linalg.norm(force_field[int(yCoord), int(xCoord), int(zCoord)] / mass))
+    # take mean of array and append it to the radial acc array
+    radial_acc_mean.append(np.mean(radial_acc_ind))
+
+# plot results
+fig, ax = plt.subplots(figsize=(20, 10))
+
+sns.lineplot(z_scan, radial_acc_mean, label='Z-Axis Sweep', linewidth=3, color='b')
+
+# formatting
+plt.title('Z-Axis Scan of Lens Acceleration Profile at Radius {}'.format(radius), fontsize=25)
+plt.ylabel(r'Mean Acceleration $\left( \frac{m}{s^2} \right)$', fontsize=20)
+plt.xlabel(r'z-axis distance ($m$)', fontsize=20)
+plt.tick_params(axis='both', labelsize=13)
+
+# save to file
+Path('{}/acc_mag_plots_{}'.format(datetime.date.today(), datetime.date.today())).mkdir(parents=True, exist_ok=True)
+plt.savefig('{}/acc_mag_plots_{}/fix_radius_z_scan_{}'.format(datetime.date.today(), datetime.date.today(), datetime.date.today()))
+
--- zeeman_sisyphus/force_mag_plot.py
+++ zeeman_sisyphus/force_mag_plot.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Vector Fields
+# Magnitude of Force at
 # ----------------------------------------------------------------------------
 from vector_field import *
 
@@ -8,11 +8,13 @@ force_x = []
 force_y = []
 
 # initialize mesh values
-mesh = range(0, m)
+mesh = range(0, mxy)
 
 # loop through mesh grid of force field
+z_pos = 10
+
 for i in mesh:
-    force_x.append(force_field[i, 0, int(m/2)])
+    force_x.append(force_field[i, 0, z_pos])
 
 # transform to numpy arrays
 force_x = np.array(force_x)
@@ -20,8 +22,6 @@ force_x = np.array(force_x)
 # get components of vectors
 force_x_plt = [force_x[i, 0] for i in range(len(mesh))]
 force_y_plt = [force_x[i, 1] for i in range(len(mesh))]
-
-breakpoint()
 
 # plot results
 fig, ax = plt.subplots(2, 1, figsize=(20, 10))
@@ -34,7 +34,9 @@ ax[1].plot(mesh, force_y_plt, label='y-component scan', linewidth=3, color='r')
 ax[0].legend()
 ax[1].legend()
 
+# title
+fig.suptitle('Force Field Scan Along y-Axis, z_pos={}'.format(z_pos), fontsize=15)
+
 # save to file
 Path('{}/force_mag_plots_{}'.format(datetime.date.today(), datetime.date.today())).mkdir(parents=True, exist_ok=True)
 plt.savefig('{}/force_mag_plots_{}/force_mag_{}'.format(datetime.date.today(), datetime.date.today(), datetime.date.today()))
-
