d3b23ff40b4388bfb06604206c5590fced94066c
Andrew-wi <andrew.winnicki.sc@gmail.com>
2021-02-13 10:39:12 -1000

    updated 35 m/s desired velclass runs

--- zeeman_sisyphus/dependencies.py
+++ zeeman_sisyphus/dependencies.py
@@ -22,8 +22,8 @@ from pathlib import Path
 
 t = 0.0
 n = 1e7 # < -- CaOH/CaF number of molecules
-mol_run = 'CaOH_scan2d_des30'
-desired_vel_class_vz = 30 # units: m/s
+mol_run = 'CaOH_scan2d_des35'
+desired_vel_class_vz = 35 # units: m/s
 sigma_xy = 0.0042 # <-- CaOH
 # sigma_vxy = 12.06 # <-- CaOH, He3 bg
 sigma_vxy = 12.0 # <-- CaOH, regparams
